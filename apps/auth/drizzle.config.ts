ba5385d0aa774b209312a76184951fcc891823ff
Ayush Bommana <bommanaayush07@gmail.com>
2026-09-19 13:10:23 +0530

    fix: remove common lib

--- apps/auth/drizzle.config.ts
+++ apps/auth/drizzle.config.ts
@@ -1,11 +1,14 @@
-import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
+config({ path: resolve(__dirname, '.env') }); // loads apps/auth/.env only
 
 export default defineConfig({
   schema: './src/database/schemas/index.ts',
   out: './src/database/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.AUTH_DATABASE_URL!,
+    url: process.env.DRIZZLE_URI!,
   },
 });
